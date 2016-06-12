7e4fa9caa05aa598261d3c836bcd30fe53416843
DiMuNiET <dimuniet82@gmail.com>
2016-06-12 21:17:16 +0200

    Afegides opcions Minimum a Set Detection Limit, modificada finestra i accions

--- src/coda/gui/menu/SetDetectionLimitMenu.java
+++ src/coda/gui/menu/SetDetectionLimitMenu.java
@@ -24,30 +24,33 @@
  */
 package coda.gui.menu;
 
-import coda.DataFrame;
-import coda.Element;
-import coda.Variable;
-import coda.Zero;
+import coda.*;
 import coda.gui.CoDaPackMain;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+
+import javax.swing.*;
 
 /**
  *
  * @author david
  */
-public class SetDetectionLimitMenu extends AbstractMenuDialog{
+public class SetDetectionLimitMenu extends AbstractMenuDialogSDL{
     public static final long serialVersionUID = 1L;
     //Fixem valor per defecte Detection Limit
     double detectionLimit = 0.01;
     //Creem el quadre de text
     JTextField usedDetectionLimit;
     //Creem el nom que tindrà el quade de text
-    JLabel l_usedDetectionLimit = new JLabel("Detection Limit");
+    JRadioButton l_usedDetectionLimit = new JRadioButton("Detection Limit");
+    //Creem la opció Take Minimun of each column
+    JRadioButton l_takeMinimunColum = new JRadioButton("Take Minimum of Each Column");
+    //Creem la opció Global of Selected Columns
+    JRadioButton l_globalSelectedColumns = new JRadioButton("Global Minimum of Selected Columns");
     
     public SetDetectionLimitMenu(CoDaPackMain mainApp) {
         super(mainApp, "Set Detection Limit", false);
-        
+
+        //Marquem el botó Detection Limit com a seleccionat per defecte
+        l_usedDetectionLimit.setSelected(true);
         //assignem mida al quadre de text
         usedDetectionLimit =  new JTextField(5);
         //assignem valor per defecte al quadre de text
@@ -56,6 +59,16 @@ public class SetDetectionLimitMenu extends AbstractMenuDialog{
         optionsPanel.add(l_usedDetectionLimit);
         //posem el quadre de text
         optionsPanel.add(usedDetectionLimit);
+        //posem la opció take minimum
+        optionsPanel.add(l_takeMinimunColum);
+        //posem la opció global minimum
+        optionsPanel.add(l_globalSelectedColumns);
+
+        //Afegir els botons a un grup per a impedir que s'en pugui seleccionar més d'un
+        ButtonGroup group = new ButtonGroup();
+        group.add(l_usedDetectionLimit);
+        group.add(l_takeMinimunColum);
+        group.add(l_globalSelectedColumns);
         
     }
 
@@ -63,9 +76,11 @@ public class SetDetectionLimitMenu extends AbstractMenuDialog{
 
     @Override
     public void acceptButtonActionPerformed() {
-        //Copiem el valor del Detection Limit
-        detectionLimit = Double.parseDouble(usedDetectionLimit.getText());
-        
+        //Creem tres booleans per a saber la opció triada
+        boolean isDetectionSelected = l_usedDetectionLimit.isSelected();
+        boolean isTakeMinimumSelected = l_takeMinimunColum.isSelected();
+        boolean isGlobalMinimumSelected = l_globalSelectedColumns.isSelected();
+
         //Obtenim el DataFrame actiu
         DataFrame df = mainApplication.getActiveDataFrame();
         //Guardem el nom de les columnes seleccionades en una taula d'strings
@@ -74,22 +89,92 @@ public class SetDetectionLimitMenu extends AbstractMenuDialog{
         int m = sel_names.length; //m correspon al numero de columnes de data[][]
         //Obtenim el numero de files de cada columna
         int f=df.getMaxVariableLength();
-        //Fem un recorregut per a cada columna del dataframe que s'ha seleccionat
-        for(int i=0;i<m;i++) {
-            //Obtenim l'array de doubles corresponent a cada columna seleccionada
-            Variable var = df.get(sel_names[i]);
-            //Fem un recorregut per totes les files de la columna seleccionada
-            for(int j=0;j<f;j++) {
-                //Carreguem cada element fila a fila de la columna seleccionada
-                Element el = var.get(j);
-                //Comprovem si es tracta d'una instància de Zero
-                if (el instanceof Zero) {
-                    //Si és un Zero li afegim el Detection Limit
-                    df.get(sel_names[i]).set(j, new Zero(detectionLimit));
+
+        //Valorem el cas que s'hagi trial la opció Detection Limit
+        if (isDetectionSelected) {
+            detectionLimit = Double.parseDouble(usedDetectionLimit.getText());
+            System.out.println("S'ha seleccionat Detection Limit amb el valor: "+detectionLimit);
+            //Fem un recorregut per a cada columna del dataframe que s'ha seleccionat
+            for(int i=0;i<m;i++) {
+                //Obtenim l'array de doubles corresponent a cada columna seleccionada
+                Variable var = df.get(sel_names[i]);
+                //Fem un recorregut per totes les files de la columna seleccionada
+                for(int j=0;j<f;j++) {
+                    //Carreguem cada element fila a fila de la columna seleccionada
+                    Element el = var.get(j);
+                    //Comprovem si es tracta d'una instància de Zero
+                    if (el instanceof Zero) {
+                        //Si és un Zero li afegim el Detection Limit
+                        df.get(sel_names[i]).set(j, new Zero(detectionLimit));
+                    }
                 }
             }
         }
-        
+        else if (isTakeMinimumSelected) {
+            double takeMinimum;
+            System.out.println("S'ha selecionat la opció d'agafar com a Detection Limit el minim de cada columa");
+            //Fem un recorregut per a cada columna del dataframe que s'ha seleccionat
+            for(int i=0;i<m;i++) {
+                //Obtenim l'array de doubles corresponent a cada columna seleccionada
+                Variable var = df.get(sel_names[i]);
+                //Fem un recorregut per totes les files de la columna seleccionada
+                takeMinimum = 2000000000.05;
+                for(int j=0;j<f;j++) {
+                    //Carreguem cada element fila a fila de la columna seleccionada
+                    Numeric el = (Numeric) var.get(j);
+                    //Comprovem si es tracta d'una instància de Zero
+                    if (el instanceof Zero) {
+
+                    }
+                    else if (el.getValue() < takeMinimum) takeMinimum = el.getValue();
+                }
+                System.out.println("El valor minim de la columna "+sel_names[i]+" es: "+takeMinimum);
+                for(int k=0;k<f;k++) {
+                    //Carreguem cada element fila a fila de la columna seleccionada
+                    Element el = var.get(k);
+                    //Comprovem si es tracta d'una instància de Zero
+                    if (el instanceof Zero) {
+                        //Si és un Zero li afegim el Detection Limit
+                        df.get(sel_names[i]).set(k, new Zero(takeMinimum));
+                    }
+                }
+            }
+        }
+        else if (isGlobalMinimumSelected) {
+            double globalMinimum = 2000000000.05;
+            System.out.println("S'ha selecionat la opció d'agafar com a Detection Limit el minim global");
+            //Fem un recorregut per a cada columna del dataframe que s'ha seleccionat
+            for(int i=0;i<m;i++) {
+                //Obtenim l'array de doubles corresponent a cada columna seleccionada
+                Variable var = df.get(sel_names[i]);
+                //Fem un recorregut per totes les files de la columna seleccionada
+                for(int j=0;j<f;j++) {
+                    //Carreguem cada element fila a fila de la columna seleccionada
+                    Numeric el = (Numeric) var.get(j);
+                    //Comprovem si es tracta d'una instància de Zero
+                    if (el instanceof Zero) {
+
+                    }
+                    else if (el.getValue() < globalMinimum) globalMinimum = el.getValue();
+                }
+            }
+            System.out.println("El valor minim global es: "+globalMinimum);
+            for(int k=0;k<m;k++) {
+                //Obtenim l'array de doubles corresponent a cada columna seleccionada
+                Variable var = df.get(sel_names[k]);
+                //Fem un recorregut per totes les files de la columna seleccionada
+                for(int l=0;l<f;l++) {
+                    //Carreguem cada element fila a fila de la columna seleccionada
+                    Element el = var.get(l);
+                    //Comprovem si es tracta d'una instància de Zero
+                    if (el instanceof Zero) {
+                        //Si és un Zero li afegim el Detection Limit
+                        df.get(sel_names[k]).set(l, new Zero(globalMinimum));
+                    }
+                }
+            }
+        }
+
         //Actualitzem el DataFrame
         mainApplication.updateDataFrame(df);
        
