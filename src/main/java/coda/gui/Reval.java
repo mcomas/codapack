705f20c10a9bb3476715fe5a44c6cc421be8c1a1
mcomas <marc.comas@gmail.com>
2017-07-05 18:06:14 +0200

    R evaluation from file2

--- src/main/java/coda/gui/Reval.java
+++ src/main/java/coda/gui/Reval.java
@@ -7,6 +7,9 @@ package coda.gui;
 
 import coda.DataFrame;
 import coda.gui.menu.ALRMenu;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.StringJoiner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,12 +25,11 @@ import org.renjin.sexp.Vector;
  * @author marcc
  */
 public class Reval {
-    static ScriptEngineManager manager = new ScriptEngineManager();
-    static final ScriptEngine engine = manager.getEngineByName("Renjin");
+    static ScriptEngine engine = new ScriptEngineManager().getEngineByName("Renjin");
     public Reval(){
         
     }
-    public static double[][] alr(DataFrame dataframe, String vnames[]){
+    public double[][] alr(DataFrame dataframe, String vnames[]){
         int K = vnames.length;
         StringJoiner joiner = new StringJoiner(",");
         if(engine == null) {
@@ -39,10 +41,44 @@ public class Reval {
                 engine.put("x" + (i+1), new DoubleArrayVector(dataframe.get(vnames[i]).getNumericalData()));
                 joiner.add("x" + (i+1));
             }
-            engine.eval("X = cbind(" + joiner.toString() + ")");
-            engine.eval("logX <- log(X)");
-            res = (Vector)engine.eval(String.format("logX[,-%d] - logX[,%d]", K, K));
-            } catch (ScriptException ex) {
+            InputStream in = getClass().getResourceAsStream(CoDaPackConf.RESOURCE_PATH + "alr.R");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            engine.eval(reader);
+            res = (Vector)engine.eval("alr(cbind(" + joiner.toString() + "))");
+        } catch (ScriptException ex) {
+            Logger.getLogger(ALRMenu.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Matrix matrix = null;
+        try {
+            matrix = new Matrix(res);
+        } catch(IllegalArgumentException e) {
+            System.out.println("Result is not a matrix: " + e);
+        }
+        double result[][] = new double[matrix.getNumCols()][matrix.getNumRows()];
+        for(int i = 0; i < matrix.getNumRows(); i++){
+            for(int j = 0; j < matrix.getNumCols(); j++){
+                result[j][i] = matrix.getElementAsDouble(i, j);
+            }
+        }
+        return result;
+    }
+    public double[][] alrInv(DataFrame dataframe, String vnames[]){
+        int K = vnames.length;
+        StringJoiner joiner = new StringJoiner(",");
+        if(engine == null) {
+            throw new RuntimeException("Renjin Script Engine not found on the classpath.");
+        }
+        Vector res = null;
+        try {
+            for(int i = 0; i < K; i++){
+                engine.put("x" + (i+1), new DoubleArrayVector(dataframe.get(vnames[i]).getNumericalData()));
+                joiner.add("x" + (i+1));
+            }
+            InputStream in = getClass().getResourceAsStream(CoDaPackConf.RESOURCE_PATH + "alr.R");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            engine.eval(reader);
+            res = (Vector)engine.eval("alrInv(cbind(" + joiner.toString() + "))");
+        } catch (ScriptException ex) {
             Logger.getLogger(ALRMenu.class.getName()).log(Level.SEVERE, null, ex);
         }
         Matrix matrix = null;
