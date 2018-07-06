559f9c09526ab75c750ad97b4b04b36e4c174946
daniiel_re <u1933604@campus.udg.edu>
2018-07-06 11:23:50 +0200

    · Exportació en format png afegit per poder guardar gràfic on vulgui l'usuari

--- src/main/java/coda/gui/menu/ZpatternsMenu.java
+++ src/main/java/coda/gui/menu/ZpatternsMenu.java
@@ -30,15 +30,26 @@ import static coda.gui.CoDaPackMain.outputPanel;
 import coda.gui.output.OutputElement;
 import coda.gui.output.OutputForR;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagLayout;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import org.rosuda.JRI.Rengine;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
 
 /**
  *
@@ -47,9 +58,7 @@ import javax.swing.JLabel;
 public class ZpatternsMenu extends AbstractMenuDialog{
     
     Rengine re;
-    JFrame plotPNG = new JFrame();
-    JPanel panel = new JPanel();
-    Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    JFrame frameZPatterns;
     
     public static final long serialVersionUID = 1L;
     
@@ -84,26 +93,9 @@ public class ZpatternsMenu extends AbstractMenuDialog{
                 
                 setVisible(false);
                 
-                        // jframe configuration
-        
-                panel.setSize(800,800);
-                plotPNG.setTitle("zPatterns Plot Output");
-                plotPNG.setIconImage(
-                Toolkit.getDefaultToolkit().
-                        getImage(
-                getClass().getResource(CoDaPackMain.RESOURCE_PATH + "logoL.png")));
-                ImageIcon icon = new ImageIcon("out.png");
-                JLabel label = new JLabel(icon,JLabel.CENTER);
-                label.setSize(700,700);
-                panel.setLayout(new GridBagLayout());
-                panel.add(label);
-                plotPNG.getContentPane().add(panel);
-                plotPNG.setSize(800,800);
-                plotPNG.setLocation(dim.width/2-plotPNG.getSize().width/2, dim.height/2-plotPNG.getSize().height/2);
-                plotPNG.setVisible(true);
+                // jframe configuration
                 
-                File file = new File("out.png");
-                file.delete();
+                plotZPatterns();
                 
                 // we show the output 
                 
@@ -112,6 +104,120 @@ public class ZpatternsMenu extends AbstractMenuDialog{
         }
         else{ // no data selected
             JOptionPane.showMessageDialog(frame, "Please select data");
+        }
+    }
+
+    private void plotZPatterns() {
+            Font f = new Font("Arial", Font.PLAIN,12);
+            UIManager.put("Menu.font", f);
+            UIManager.put("MenuItem.font",f);
+            JMenuBar menuBar = new JMenuBar();
+            JMenu menu = new JMenu("File");
+            JMenuItem menuItem = new JMenuItem("Open");
+            menuBar.add(menu);
+            frameZPatterns = new JFrame();
+            JPanel panel = new JPanel();
+            menu.add(menuItem);
+            menuItem = new JMenuItem("Export");
+            JMenu submenuExport = new JMenu("Export");
+            menuItem = new JMenuItem("Export As PNG");
+            menuItem.addActionListener(new FileChooserAction());
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Export As JPEG");
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Export As PDF");
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Export As WMF");
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Export As Postscripts");
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Quit");
+            menuItem.addActionListener(new quitListener());
+            menu.add(submenuExport);
+            menu.add(menuItem);
+            frameZPatterns.setJMenuBar(menuBar);
+            panel.setSize(800,800);
+            ImageIcon icon = new ImageIcon("out.png");
+            JLabel label = new JLabel(icon,JLabel.CENTER);
+            label.setSize(700, 700);
+            panel.setLayout(new GridBagLayout());
+            panel.add(label);
+            frameZPatterns.getContentPane().add(panel);
+            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            frameZPatterns.setSize(800,800);
+            frameZPatterns.setLocation(dim.width/2-frameZPatterns.getSize().width/2, dim.height/2-frameZPatterns.getSize().height/2);
+            
+            WindowListener exitListener = new WindowAdapter(){
+                
+                @Override
+                public void windowClosing(WindowEvent e){
+                    int confirm = JOptionPane.showOptionDialog(null,"Are You Sure to Close Window?","Exit Confirmation", JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,null,null);
+                    if(confirm == 0){
+                        frameZPatterns.dispose();
+                        File file = new File("out.png");
+                        file.delete();
+                    }
+                }
+            };
+            
+            frameZPatterns.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            frameZPatterns.addWindowListener(exitListener);
+            frameZPatterns.setVisible(true);
+    }
+    
+    private class quitListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            int confirm = JOptionPane.showOptionDialog(null,"Are You Sure to Close Window?","Exit Confirmation", JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,null,null);
+            if(confirm == 0){
+                frameZPatterns.dispose();
+                File file = new File("out.png");
+                file.delete();
+            }
+        }
+    }
+    
+    private class FileChooserAction implements ActionListener{
+        
+        public void actionPerformed(ActionEvent e){
+            JFrame frame = new JFrame();
+            JFileChooser jf = new JFileChooser();
+            frame.setSize(400,400);
+            jf.setDialogTitle("Select the folder to save the file");
+            jf.setApproveButtonText("Save");
+            jf.setSelectedFile(new File(".png"));
+            jf.setSize(400,400);
+            frame.add(jf);
+            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+            boolean canExit = false;
+            while(! canExit){
+                int result = jf.showSaveDialog(frame);
+                if(JFileChooser.CANCEL_OPTION == result){
+                    frame.dispose();
+                    canExit = true;
+                }
+                if(JFileChooser.APPROVE_OPTION == result){ // guardem arxiu en el path
+                    File f = new File("out.png");
+                    f.deleteOnExit();
+                    String path = jf.getSelectedFile().getAbsolutePath();
+                    File f2 = new File(path);
+                    if(f2.exists()){
+                        int response = JOptionPane.showConfirmDialog(null, "Do you want to replace the existing file?", 
+                                "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                        if(response != JOptionPane.YES_OPTION) canExit = false;
+                        else{
+                            f.renameTo(f2);
+                            frame.dispose();
+                            canExit = true;
+                        }
+                    }
+                    else{
+                        f.renameTo(f2);
+                        frame.dispose();
+                        canExit = true;
+                    }
+                }
+            }
         }
     }
 }
