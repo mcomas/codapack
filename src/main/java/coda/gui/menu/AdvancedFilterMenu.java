209eac0a34d159c308859f2b88415977d77386a0
daniiel_re <u1933604@campus.udg.edu>
2018-07-12 10:53:24 +0200

    · Filtratge avançat amb R acabat

--- src/main/java/coda/gui/menu/AdvancedFilterMenu.java
+++ src/main/java/coda/gui/menu/AdvancedFilterMenu.java
@@ -9,6 +9,9 @@ import coda.DataFrame;
 import coda.gui.CoDaPackMain;
 import coda.io.ImportRDA;
 import java.awt.FlowLayout;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -17,6 +20,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import org.rosuda.JRI.REXP;
 import org.rosuda.JRI.Rengine;
 
 /**
@@ -33,12 +37,15 @@ public class AdvancedFilterMenu{
             JOptionPane.showMessageDialog(null, "No data available");
         }
         else{ // fem el filtratge
+            DataFrame df = mainApp.getActiveDataFrame();
             boolean exit = false;
             re = r;
             while(!exit){
                 JTextField expressionField = new JTextField(20);
                 JPanel myPanel = new JPanel();
-                myPanel.add(new JLabel("Expression:"));
+                JLabel labelMessage = new JLabel("Use x1 to x" + String.valueOf(df.getNames().size()) + " names values for variables");
+                JOptionPane.showMessageDialog(null, labelMessage);
+                myPanel.add(new JLabel("R expression to subset:"));
                 myPanel.add(expressionField);
                 int answer = JOptionPane.showConfirmDialog(null,myPanel, "Advanced Filter", JOptionPane.OK_CANCEL_OPTION);
 
@@ -49,69 +56,83 @@ public class AdvancedFilterMenu{
                     String expression = expressionField.getText();
 
                     if(expression.length() != 0){
-                        exit = true;
-                        
-                        DataFrame df = mainApp.getActiveDataFrame();
-                        /*double[] auxNumeric = null;
-                        String[] auxText = null;
-                        
-                        if(df.get(0).isNumeric()){
-                            auxNumeric = df.get(0).getNumericalData();
-                        }
-                        else{
-                            auxText = df.get(0).getTextData();
-                        }*/
-                
-                        /*if(df.get(0).isNumeric()) re.assign("X", auxNumeric);
-                        else re.assign("X", auxText);
-                        
-                        re.eval("X" + " <- matrix( " + "X" + " ,nc=1)");
-                        for(int i=1; i < df.getNames().size(); i++){
-                            if(df.get(i).isNumeric()){
-                                auxNumeric = df.get(i).getNumericalData();
-                            }
-                            else{
-                                auxText = df.get(i).getTextData();
-                            }
-                            if(df.get(i).isNumeric()) re.assign("tmp", auxNumeric);
-                            else re.assign("tmp",auxText);
-                            re.eval("X" + " <- cbind(" + "X" + ",matrix(tmp,nc=1))");
-                        }*/
                         
                         // create dataframe on r
                         
                         for(int i=0; i < df.getNames().size();i++){ // totes les columnes
-                            re.eval("c" + String.valueOf(i+1) + " <- NULL");
+                            re.eval("x" + String.valueOf(i+1) + " <- NULL");
                             if(df.get(i).isNumeric()){
                                 for(double j : df.get(i).getNumericalData()){
-                                    re.eval("c" + String.valueOf(i+1) + " <- c(c" + String.valueOf(i+1) +"," + String.valueOf(j) + ")");
+                                    re.eval("x" + String.valueOf(i+1) + " <- c(x" + String.valueOf(i+1) +"," + String.valueOf(j) + ")");
                                 }
                             }
                             else{
                                 for(String j : df.get(i).getTextData()){
-                                    re.eval("c" + String.valueOf(i+1) + " <- c(c" + String.valueOf(i+1) +",'" + j + "')");
+                                    re.eval("x" + String.valueOf(i+1) + " <- c(x" + String.valueOf(i+1) +",'" + j + "')");
                                 }
                             }
                         }
                         
                         String dataFrameString = "mydf <- data.frame(";
                         for(int i=0; i < df.getNames().size();i++){
-                            dataFrameString += "c" + String.valueOf(i+1);
+                            dataFrameString += "x" + String.valueOf(i+1);
                             if(i != df.getNames().size()-1) dataFrameString += ",";
                         }
                         
                         dataFrameString +=")";
                         
-                        for(int i=0; i < df.getNames().size();i++){
-                            re.eval("out <- capture.output(c" + String.valueOf(i+1) + ")");
-                            String[] out = re.eval("out").asStringArray();
-                            for(String j : out) System.out.println(j);
-                        }
+                        re.eval(dataFrameString); // we create the dataframe in R
                         
-                        re.eval(dataFrameString);
-                        re.eval("out <- capture.output(mydf)");
-                        String[] out = re.eval("out").asStringArray();
-                        String aux = "hola";
+                        // cal fer el subset
+                        
+                        re.eval ("newdata <- subset(mydf," + expression + ")");
+                        
+                       REXP rexp = re.eval("out <- capture.output(newdata)");
+                        
+                        if(rexp != null){
+                            
+                            exit = true;
+                        
+                            String[] out = re.eval("out").asStringArray();
+                            
+                            if(!out[1].equals("<0 rows> (or 0-length row.names)")){
+                        
+                                int numberofdata  = df.getMaxVariableLength(); // numero de files
+                                Vector<Integer> validRows = new Vector<Integer>();
+                                Vector<Integer> rowsToDelete = new Vector<Integer>();
+
+                                for(int i = 1; i < out.length; i++){
+                                    Matcher matcher = Pattern.compile("\\d+").matcher(out[i]);
+                                    matcher.find();
+                                    validRows.add(Integer.valueOf(matcher.group())-1);
+                                }
+
+                                for(int i=0; i < numberofdata; i++){
+                                    if(!validRows.contains(i)) rowsToDelete.add(i);
+                                }
+
+                                int[] resRowsToDelete = new int[rowsToDelete.size()];
+                                for(int i=0; i < rowsToDelete.size();i++) resRowsToDelete[i] = rowsToDelete.elementAt(i);
+
+                                DataFrame filtredDataFrame = new DataFrame(df);
+                                filtredDataFrame.subFrame(resRowsToDelete);
+                                int nameNum = 0;
+                                String newDataFrameName = df.name + "advFilt" + String.valueOf(nameNum);
+                                while(!mainApp.isDataFrameNameAvailable(newDataFrameName)){
+                                    nameNum++;
+                                    newDataFrameName = df.name + "advFilt" + String.valueOf(nameNum);
+                                }
+                                filtredDataFrame.setName(newDataFrameName);
+                                mainApp.addDataFrame(filtredDataFrame);
+                            }
+                            else{
+                                JOptionPane.showMessageDialog(null, "No data for this expression");
+                            }
+                        }
+                        else{
+                            JOptionPane.showMessageDialog(null, "Invalid expression");
+                        }
+                       
                     }
                     else{
                         JOptionPane.showMessageDialog(null, "Please put some expression");
