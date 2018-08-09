c20b78b8f8852cf7342038611b75ee76239b38e4
daniiel_re <u1933604@campus.udg.edu>
2018-08-09 23:06:28 +0200

    · ScatterplotMenu afegit

---
+++ src/main/java/coda/gui/menu/ScatterplotMenu.java
@@ -0,0 +1,202 @@
+/** 
+ *  Copyright 2011-2016 Marc Comas - Santiago Thió
+ *
+ *  This file is part of CoDaPack.
+ *
+ *  CoDaPack is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CoDaPack is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CoDaPack.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package coda.gui.menu;
+
+import coda.DataFrame;
+import javax.swing.JFrame;
+import org.rosuda.JRI.Rengine;
+import coda.gui.CoDaPackMain;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Guest2
+ */
+public class ScatterplotMenu extends AbstractMenuDialog{
+    
+    Rengine re;
+    JFrame frameScatterplot;
+    String tempDirR;
+    
+    public static final long serialVersionUID = 1L;
+    
+    public Scatterplot(final CoDaPackMain mainApp, Rengine r){
+        super(mainApp, "Scatterplot Menu", false);
+        re = r;
+    }
+    
+    @Override
+    public void acceptButtonActionPerformed(){
+        
+        JFrame frame = new JFrame();
+        frame.setTitle("Message");
+        
+        String selectedNames[] = ds.getSelectedData();
+        
+        if(selectedNames.length == 2 || selectedNames.length == 3){
+            
+            DataFrame df = mainApplication.getActiveDataFrame();
+            
+            for(int i=0; i < selectedNames.length; i++){
+                re.eval("c" + String.valueOf(i+1) + " <- NULL");
+                for(double j: df.get(selectedNames[i]).getNumericalData()){
+                    re.eval("c" + String.valueOf(i+1) + " <- c(c" + String.valueOf(i+1) + "," + String.valueOf(j) + ")");
+                }
+            }
+            
+            re.eval("mypath = tempdir()");
+            tempDirR = re.eval("print(mypath)").asString();
+            tempDir += "\\out.png";
+            
+            re.eval("png(base::paste(tempdir(),\"out.png\",sep=\"\\\\\"),width=700,height=700)");
+            re.eval("png(mypath,width=700,height=700");
+            re.eval("scatterplot3d::scatterplot3d(c1,c2,c3, pch=16, highlight.3d=TRUE,type=\"h\", main = \"3D Scatterplot\")");
+            re.eval("dev.off()");
+            
+            this.dispose();
+            
+            plotScatterplot();
+            
+        }
+        else{
+            JOptionPane.showMessageDialog(frame,"Please select two or three variables");
+        }
+        
+    }
+    
+    private void plotZPatterns() {
+            Font f = new Font("Arial", Font.PLAIN,12);
+            UIManager.put("Menu.font", f);
+            UIManager.put("MenuItem.font",f);
+            JMenuBar menuBar = new JMenuBar();
+            JMenu menu = new JMenu("File");
+            JMenuItem menuItem = new JMenuItem("Open");
+            menuBar.add(menu);
+            frameZPatterns = new JFrame();
+            JPanel panel = new JPanel();
+            menu.add(menuItem);
+            menuItem = new JMenuItem("Export");
+            JMenu submenuExport = new JMenu("Export");
+            menuItem = new JMenuItem("Export As PNG");
+            menuItem.addActionListener(new FileChooserAction());
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Export As JPEG");
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Export As PDF");
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Export As WMF");
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Export As Postscripts");
+            submenuExport.add(menuItem);
+            menuItem = new JMenuItem("Quit");
+            menuItem.addActionListener(new quitListener());
+            menu.add(submenuExport);
+            menu.add(menuItem);
+            frameZPatterns.setJMenuBar(menuBar);
+            panel.setSize(800,800);
+            ImageIcon icon = new ImageIcon(tempDirR);
+            JLabel label = new JLabel(icon,JLabel.CENTER);
+            label.setSize(700, 700);
+            panel.setLayout(new GridBagLayout());
+            panel.add(label);
+            frameZPatterns.getContentPane().add(panel);
+            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            frameZPatterns.setSize(800,800);
+            frameZPatterns.setLocation(dim.width/2-frameZPatterns.getSize().width/2, dim.height/2-frameZPatterns.getSize().height/2);
+            
+            WindowListener exitListener = new WindowAdapter(){
+                
+                @Override
+                public void windowClosing(WindowEvent e){
+                    int confirm = JOptionPane.showOptionDialog(null,"Are You Sure to Close Window?","Exit Confirmation", JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,null,null);
+                    if(confirm == 0){
+                        frameZPatterns.dispose();
+                        File file = new File(tempDirR);
+                        file.delete();
+                    }
+                }
+            };
+            
+            frameZPatterns.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            frameZPatterns.addWindowListener(exitListener);
+            frameZPatterns.setVisible(true);
+    }
+    
+    private class quitListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            int confirm = JOptionPane.showOptionDialog(null,"Are You Sure to Close Window?","Exit Confirmation", JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,null,null);
+            if(confirm == 0){
+                frameZPatterns.dispose();
+                File file = new File(tempDirR);
+                file.delete();
+            }
+        }
+    }
+    
+    private class FileChooserAction implements ActionListener{
+        
+        public void actionPerformed(ActionEvent e){
+            JFrame frame = new JFrame();
+            JFileChooser jf = new JFileChooser();
+            frame.setSize(400,400);
+            jf.setDialogTitle("Select the folder to save the file");
+            jf.setApproveButtonText("Save");
+            jf.setSelectedFile(new File(".png"));
+            jf.setSize(400,400);
+            frame.add(jf);
+            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+            boolean canExit = false;
+            while(! canExit){
+                int result = jf.showSaveDialog(frame);
+                if(JFileChooser.CANCEL_OPTION == result){
+                    frame.dispose();
+                    canExit = true;
+                }
+                if(JFileChooser.APPROVE_OPTION == result){ // guardem arxiu en el path
+                    File f = new File(tempDirR);
+                    f.deleteOnExit();
+                    String path = jf.getSelectedFile().getAbsolutePath();
+                    File f2 = new File(path);
+                    if(f2.exists()){
+                        int response = JOptionPane.showConfirmDialog(null, "Do you want to replace the existing file?", 
+                                "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                        if(response != JOptionPane.YES_OPTION) canExit = false;
+                        else{
+                            f.renameTo(f2);
+                            frame.dispose();
+                            canExit = true;
+                        }
+                    }
+                    else{
+                        f.renameTo(f2);
+                        frame.dispose();
+                        canExit = true;
+                    }
+                }
+            }
+        }
+    }
+}
