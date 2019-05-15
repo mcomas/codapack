bf821913f372486a940bc689dd515954ce2523af
daniiel_re <u1933604@campus.udg.edu>
2019-05-15 21:24:00 +0200

    GeometricMeanBarplot corregit

--- Scripts_Amb_Base/scripGeometricMeanBarplot (FET).R
+++ Scripts_Amb_Base/scripGeometricMeanBarplot (FET).R
@@ -7,9 +7,12 @@
 
 
 #load("L:/CoDaCourse/CoDaCourse 17/Material/LABS 2017/Scripts 2018/CoDaLabs/Scripts CoDaPack/activManova.RData") #name = nom de l'arxiu de dades
+#load("L:/CoDaCourse/CoDaCourse 17/Material/LABS 2017/Scripts 2018/CoDaLabs/Scripts CoDaPack/DataFAM.RData") #name = nom de l'arxiu de dades
 
 #X <- as.data.frame(dpolen[,1:3])
 #Y <- as.data.frame(dpolen[,4])
+#X <- as.data.frame(dFAM[,1:3])
+#Y <- as.data.frame(dFAM[,4])
 if (is.factor(Y) == FALSE) {
          Y <- as.factor(as.matrix(Y))
      }
@@ -23,11 +26,13 @@ int = as.integer(Y)
 Mat = cbind.data.frame(X,int)
 nCat = nlevels(as.factor(Y)) 
   
-nRow <- choose(nCat,2)
+#nRow <- choose(nCat,2)
+nRow <- nOrig
 nCol <- nCat
 rm(cmat)
-cmat <- matrix(, nrow = choose(nCat,2), ncol = nCat)
-  
+#cmat <- matrix(, nrow = choose(nCat,2), ncol = nCat)
+cmat <- matrix(, nrow = nCat, ncol = nOrig)
+
 cmat[1,]=exp(apply(log(Mat[Mat[,nOrig+1]==1,1:nOrig]),2,mean))
 Num <- rbind(cmat[1,])
 cT <- exp(apply(log(Mat[,1:nOrig]),2,mean))
