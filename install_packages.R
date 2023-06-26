53b64acbdfdd08d2737a00d6f15cac77a0dee794
mcomas <marc.comas@gmail.com>
2023-06-26 13:55:08 +0200

    Version for Windows build

--- install_packages.R
+++ install_packages.R
@@ -1,5 +1,7 @@
 available_packages = rownames(installed.packages())
-if(!"rJava" %in% available_packages) install.packages('rJava')
-if(!"coda.base" %in% available_packages) install.packages('coda.base')
-if(!"zCompositions" %in% available_packages) install.packages('zCompositions')
-if(!"fpc" %in% available_packages) install.packages('fpc')
+R_repo = 'http://cran.us.r-project.org'
+
+if(!"rJava" %in% available_packages) install.packages('rJava', repos = R_repo)
+if(!"coda.base" %in% available_packages) install.packages('coda.base', repos = R_repo)
+if(!"zCompositions" %in% available_packages) install.packages('zCompositions', repos = R_repo)
+if(!"fpc" %in% available_packages) install.packages('fpc', repos = R_repo)
