1e255388a7129e84898f29b1b4243264afde7a3b
mcomas <marc.comas@gmail.com>
2023-11-13 15:55:27 +0100

    dependcies

--- install_packages.R
+++ install_packages.R
@@ -1,7 +1,7 @@
 available_packages = rownames(installed.packages())
 R_repo = 'http://cran.us.r-project.org'
 
-install.packages('rJava', repos = R_repo)
-install.packages('coda.base', repos = R_repo)
-install.packages('zCompositions', repos = R_repo)
-install.packages('fpc', repos = R_repo)
+install.packages('rJava', repos = R_repo, dependencies = TRUE)
+install.packages('coda.base', repos = R_repo, dependencies = TRUE)
+install.packages('zCompositions', repos = R_repo, dependencies = TRUE)
+install.packages('fpc', repos = R_repo, dependencies = TRUE)
