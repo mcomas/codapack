528a19e75c16c7a3fe338cc6b7af393f0bead91b
mcomas <marc.comas@gmail.com>
2023-11-22 16:47:09 +0100

    Script to create windows launch4j

--- run-codapack.bat
+++ run-codapack.bat
@@ -2,5 +2,6 @@ set PATH=R-4.3.1\bin\x64;Rlibraries\rJava\jri;%PATH%
 set JAVA_HOME=jdk-17.0.7
 set R_HOME=R-4.3.1
 set R_LIBS_USER=Rlibraries
+set CDP_R_SCRIPTS=Rscripts
 
 jdk-17.0.7\bin\java -classpath target\classes;target\dependency\* coda.gui.CoDaPackMain
